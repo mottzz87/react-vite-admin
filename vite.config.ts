e9005196b41d03075f6cb13ca19c9ffa3546bd82
vane <hualuna87@126.com>
2021-09-24 02:00:35 +0800

    t

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 /*
  * @Author: Vane
  * @Date: 2021-08-28 00:52:36
- * @LastEditTime: 2021-09-02 21:03:41
+ * @LastEditTime: 2021-09-24 01:57:03
  * @LastEditors: Vane
  * @Description:
- * @FilePath: \react-vite\vite.config.ts
+ * @FilePath: \react-vite-admin\vite.config.ts
  */
 import { resolve } from 'path';
 import dayjs from 'dayjs';
@@ -39,6 +39,8 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
 
 	// The boolean type read by loadEnv is a string. This function can be converted to boolean type
 	const viteEnv = wrapperEnv(env);
+
+	console.log(1111, env, mode, root);
 
 	const {
 		VITE_PORT,
